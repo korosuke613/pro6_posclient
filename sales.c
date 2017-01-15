041ae6d4ffe3adbf8198ad5f2a16a28f07a5d261
Futa Hirakoba <korosuke613613@gmail.com>
2017-01-16 02:17:22 +0900

    サーバー側からも商品を削除できるようにした(1コメだけ)

--- sales.c
+++ sales.c
@@ -257,11 +257,17 @@ G_MODULE_EXPORT void cb_sales3_exec(GtkButton *button, gpointer data){
 }
 
 G_MODULE_EXPORT void cb_sales1_tree_delete(GtkButton *button, gpointer data){
+    char sendBuf[BUFSIZE], recvBuf[BUFSIZE_MAX];
+    int sendLen, recvLen, recordCount=0;
+    char *records[RECORD_MAX], response[BUFSIZE], param[9][BUFSIZE];
+    int i;
     //テスト用リスト削除コード
     GtkListStore *store;
     GtkTreeSelection *selection;
     GtkTreeIter iter;
     gboolean success;
+    GtkTreeModel *model;
+    int listNum;
 
     selection = gtk_tree_view_get_selection(salesHData->productTree);
     if(!selection)return;
@@ -269,8 +275,30 @@ G_MODULE_EXPORT void cb_sales1_tree_delete(GtkButton *button, gpointer data){
     store = GTK_LIST_STORE(gtk_tree_view_get_model(salesHData->productTree));
     success = gtk_tree_selection_get_selected(selection, NULL, &iter);
     if(success)gtk_list_store_remove(store, &iter);
-}
+    /*
+    model = GTK_TREE_MODEL(gtk_tree_view_get_model(salesHData->productTree));
+    gtk_tree_model_get(model, &iter, 0, &listNum, -1);
+      */
 
+    if(g_soc>0){
+        sendLen = sprintf(sendBuf, "%s %s %s %s","CORRECT", "1", "0",ENTER);
+        send(g_soc, sendBuf, sendLen, 0);
+        recvLen=recv_data(g_soc, recvBuf, BUFSIZE_MAX);
+        recordCount=record_division(recvBuf, records);
+        memset(response,0,BUFSIZE);
+        for(i=0;i<9;i++){
+            memset(param[i],0,BUFSIZE);
+        }
+        /* レスポンスメッセージを解析 */
+        sscanf(records[0], "%s %s %s %s %s %s %s", response, param[0], param[1], param[2], param[3], param[4], param[5]);
+
+        if(strcmp(response, OK_STAT) != 0){
+            /* エラーメッセージを表示 */
+            showSalesErrorMsg(salesHData->pointresultLabel, atoi(param[1]));
+            return;
+        }
+    }
+}
 /**
  * ログインエラーメッセージ表示
  * showErrorMsg 
