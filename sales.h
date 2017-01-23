7986b50ccf674459704c028f4baf28e486b70dbd
Futa Hirakoba <korosuke613@users.noreply.github.com>
2017-01-23 15:55:28 +0900

    結果画面に行く前に、警告画面を出すように追加

--- sales.h
+++ sales.h
@@ -38,6 +38,8 @@ typedef struct _salesHandleData{
 	GtkWidget  *salesWindow;           //販売会計ウィンドウ１
 	GtkWidget  *breakDialog;			//breakDialogウィンドウ
 	GtkWidget  *pointcardWindow;
+	GtkWidget  *resultWindow;
+	GtkWidget  *resultdangerDialog;
 	GtkButton  *cancelButton1;          //戻るボタン
 	GtkButton  *cancelButton3;			//breakDialogのいいえボタン
 	GtkButton  *nextButton;          //次へボタン
